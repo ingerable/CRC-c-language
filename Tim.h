11ae1994ca732da2bdd7074904649a33c86b6fbd
Timothee Soen <timothee.soen@unistra.fr>
2017-01-05 14:43:58 +0100

    Division euclidienne de deux polynomes

---
+++ Tim.h
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdint.h>
+
+uint32_t division(uint32_t nombre, uint32_t diviseur);
+int get_nb_bits(int nombre);
+int division_binaire(uint8_t nombre, uint8_t diviseur);
